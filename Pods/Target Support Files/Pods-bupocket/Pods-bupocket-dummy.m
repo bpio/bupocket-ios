b55668c5d5f94531d46b027f9ce4ac75ebaa8057
huoss <huoshuangshaung@bubi.cn>
2018-11-15 14:51:24 +0800

    update .gitignore

--- Pods/Target Support Files/Pods-bupocket/Pods-bupocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bupocket : NSObject
-@end
-@implementation PodsDummy_Pods_bupocket
-@end
--- Pods/Target Support Files/Pods-bupocketTests/Pods-bupocketTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bupocketTests : NSObject
-@end
-@implementation PodsDummy_Pods_bupocketTests
-@end
